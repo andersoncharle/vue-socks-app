d4baab4ea6e960f0484bf47716119e75e0fc6281
blackmitnick <mkakarianderson68@.com>
2022-03-17 21:59:09 +0300

    fifth commit

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const product = 'Socks'
+const product = 'socks'
 
 const app = Vue.createApp({
     // Inorder to simplify the syntax ES6 shorthand are used instead.
     // so shorthand syntax is data(){ return { data: 'value' }}
     data: function() {
         return{
-            message: product
+            product: product,
+            description: "This is socks product",
+            image: "./assets/images/socks_green.jpg" ,
+            url: 'https://github.com/blackmitnick/vue-socks-app'
+
         }
     }
 })
