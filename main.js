4c97ff2c6c3b94521546269a299dc415b0f493a8
blackmitnick <mkakarianderson68@.com>
2022-03-17 21:25:38 +0300

    thrid commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const product = 'Socks'
 
 const app = Vue.createApp({
+    // Inorder to simplify the syntax ES6 shorthand are used instead.
+    // so shorthand syntax is data(){ return { data: 'value' }}
     data: function() {
         return{
             message: product
